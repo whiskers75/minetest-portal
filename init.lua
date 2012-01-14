4f83e45aed8de269f2c0c2b533b744fcb58169ab
Nemo 08 <bigblacklazycat@gmail.com>
2012-01-14 23:16:02 +0400

    rev 0.0.1 - testing

--- init.lua
+++ init.lua
@@ -1,53 +1,449 @@
-	local save_conf_time = 20
-	local portal_list = {}
+--
+--
+-- 		Author: Nemo08
+--		Portal mod
+--
+	local version = "0.0.1"
+--
+--	
+
+	local save_conf_time = 5
+	local portals_content_conf = minetest.get_modpath('portal').."/portals_content"
 	
-	function load_portals ()
-	end
+	
+	portal_list = {}
+	halfportal_list = {} -- unfinished portals
+	dofile (minetest.get_modpath('portal') .. "/portal_list.lua")
+	local portals_changed = false
+	local rune_unactive = "portal:rune_raido_b"
+	local rune_active = "portal:rune_raido_g"
+	local rune_otherstone ="default:cobble"
+	
+	-----------------------------------------------------------------------------
+	local base = _G
+local function isValidType(valueType)
+  return "number" == valueType or 
+         "boolean" == valueType or 
+         "string" == valueType
+end
+
+-- конвертация переменной в строку
+local function valueToString (value)
+  local valueType = base.type(value)
+  
+  if "number" == valueType or "boolean" == valueType then
+    result = base.tostring(value)
+  else  -- assume it is a string
+    -- обратите внимание на флаг "%q"!
+    -- этот флаг правильно обрабатывает строки, 
+    -- содержащие в себе кавычки и другие управляющие символы
+    result = base.string.format("%q", value)
+  end
+  
+  return result
+end
+
+function portal_save (sfile,name, value, saved)
+  saved = saved or {}       -- initial value
+  sfile:write(name, " = ")
+  local valueType = base.type(value)
+  if isValidType(valueType) then
+    sfile:write(valueToString(value), "\n")
+  elseif "table" == valueType then
+    if saved[value] then    -- value already saved?
+      sfile:write(saved[value], "\n")  -- use its previous name
+    else
+      saved[value] = name   -- save name for next time
+      sfile:write("{}\n")     -- create a new table
+      for k,v in base.pairs(value) do      -- save its fields
+        -- добавляем проверку ключа таблицы
+        local keyType = base.type(k)
+        if isValidType(keyType) then
+          local fieldname = base.string.format("%s[%s]", name, valueToString(k))
+          portal_save(sfile,fieldname, v, saved)
+        else
+          base.error("cannot save a " .. keyType)
+        end
+      end
+    end
+  else
+    base.error("cannot save a " .. valueType)
+  end
+end
+	-----------------------------------------------------------------------------
 	
 	function save_portals ()
+		if portals_changed then
+			local output = io.open(minetest.get_modpath('portal').."/portal_list.lua", "w")
+			portal_save(output,"portal_list", portal_list)
+			portal_save(output,"halfportal_list", halfportal_list)
+			io.close(output)
+			--print("Сохранил порталы!!! ")
+		end
 	end	
 	
-	tp = {
-		physical = true,
-		textures ={"default_cobble.png","default_cobble.png","raido_b.png","default_cobble.png","default_cobble.png","default_cobble.png"},
+	local delta = 0
+	minetest.register_globalstep(function(dtime)
+    delta = delta + dtime
+    if delta > save_conf_time then
+        delta = 0
+		if portals_changed then
+			save_portals()
+			portals_changed = false
+		end
+    end
+end)
+	
+	
+	function build_portal(pos,placer,size,rune,stone)
+		local placer_pos = placer:getpos()
+		local dirang = -1  -- in grad, 0 grad from x counterclockwise
+		local ld = placer:get_look_dir()
+		--print(pos.x,pos.z)
+		--print(placer_pos.x,placer_pos.z)
+		for j,k in pairs(halfportal_list) do
+			if (placer.name == k.builder) then
+				size = k.size
+			end
+		end
+		
+		if math.abs(ld.x)<math.abs(ld.z) then
+			for i=0,(size-1) do
+				minetest.env:add_node({x=(pos.x - (size-1)/2), y=(pos.y+i), z=pos.z}, {name=stone})
+				minetest.env:add_node({x=(pos.x + (size-1)/2), y=(pos.y+i), z=pos.z}, {name=stone})
+				minetest.env:add_node({x=(pos.x - (size-1)/2+i), y=(pos.y+(size-1)), z=(pos.z)}, {name=stone})
+				minetest.env:add_node({x=(pos.x - (size-1)/2+i), y=(pos.y), z=pos.z}, {name=stone})
+			end
+			minetest.env:add_node({x=pos.x, y=pos.y, z=pos.z}, {name=rune})
+			minetest.env:add_node({x=pos.x, y=(pos.y+(size-1)), z=pos.z}, {name=rune})
+			minetest.env:add_node({x=(pos.x - (size-1)/2), y=(pos.y + (size-1)/2), z=pos.z}, {name=rune})
+			minetest.env:add_node({x=(pos.x + (size-1)/2), y=(pos.y + (size-1)/2), z=pos.z}, {name=rune})
+			
+			dirang = 180 + (math.abs(ld.z) / ld.z) * 90
+		else
+			for i=0,(size-1) do
+				minetest.env:add_node({x=pos.x, y=(pos.y+i), z=(pos.z - (size-1)/2)}, {name=stone})
+				minetest.env:add_node({x=pos.x, y=(pos.y+i), z=(pos.z + (size-1)/2)}, {name=stone})
+				minetest.env:add_node({x=pos.x, y=(pos.y+(size-1)), z=(pos.z - (size-1)/2+i)}, {name=stone})
+				minetest.env:add_node({x=pos.x, y=(pos.y), z=(pos.z - (size-1)/2+i)}, {name=stone})
+			end
+			minetest.env:add_node({x=pos.x, y=pos.y, z=pos.z}, {name=rune})
+			minetest.env:add_node({x=pos.x, y=(pos.y+(size-1)), z=pos.z}, {name=rune})
+			minetest.env:add_node({x=pos.x, y=(pos.y + (size-1)/2), z=(pos.z - (size-1)/2)}, {name=rune})
+			minetest.env:add_node({x=pos.x, y=(pos.y + (size-1)/2), z=(pos.z + (size-1)/2)}, {name=rune})
+			
+			dirang = 90 - (math.abs(ld.x) / ld.x) * 90
+		end
+
+			local findhalf = false
+			local placername = placer:get_player_name()
+			
+			for j, k in pairs(halfportal_list) do
+				if (placername == k.builder) then
+					--print("Нашол полупортал этого игрока! ",j," , добавляю этот и " .. pos.x .."_" .. pos.y .. "_" .. pos.z .. " в полный портал")
+					print("player "..placer:get_player_name().." build fullportal ("..pos.x..","..pos.y..","..pos.z..")->("..k.myx..","..k.myy..","..k.myz..")")
+					portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] = { 	builder = placername, --this portal add to portal_list
+																			type ="public",
+																			size = size,
+																			exname = j,
+																			state = "built",
+																			dir = dirang,
+																			myx = pos.x,
+																			myy = pos.y,
+																			myz = pos.z,	
+																			}
+					portal_list[k.myx .."_" .. k.myy .. "_" .. k.myz] = { 	builder = placername, --target portal add to portal_list
+																			type ="public",
+																			size = k.size,
+																			exname = pos.x .."_" .. pos.y .. "_" .. pos.z,
+																			state = "built",
+																			dir = k.dir,
+																			myx = k.myx,
+																			myy = k.myy,
+																			myz = k.myz,	
+																			}
+					halfportal_list[j] = nil -- rem target from halfportal
+					check_portal(pos)
+					findhalf = true
+				end
+			end
+			
+			if findhalf == false then --we dont find other halfportal, add this to halfportal_list
+				--print("Не нашол полупортала этого игрока, добавляю ".. pos.x .."_" .. pos.y .. "_" .. pos.z)
+				print("player "..placer:get_player_name().." build halfportal ("..pos.x..","..pos.y..","..pos.z..")")
+				--local pinv = placer.get_inventory()
+				local pinv = placer:get_inventory()
+				pinv:autoinsert_stackstring("main",  'node "portal:rune_raido_b" 1')
+				--placer:add_to_inventory("main", 'node "portal:rune_raido_b" 1')
+				halfportal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] = { 	builder = placername, --this portal add to portal_list
+																			type ="public",
+																			size = size,
+																			exname = "no_exit!",
+																			state = "half",
+																			dir = dirang,
+																			myx = pos.x,
+																			myy = pos.y,
+																			myz = pos.z,	
+																		}
+			end
+		
+		minetest.env:add_entity({x=pos.x,y=pos.y,z=pos.z},'portal:portal_control_ent')
+		portals_changed = true
+	end
+	
+	function remove_portal(pos,entity)
+		pos.x = math.floor(pos.x)
+		pos.y = math.floor(pos.y)
+		pos.z = math.floor(pos.z)
+		local this_portal = {}
+		if portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] ~= nil then
+			this_portal = portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+		else
+			this_portal = halfportal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+		end
+		if (this_portal.dir == 90)or(this_portal.dir == 270) then
+			for i=0,(this_portal.size-1) do
+				minetest.env:remove_node({x=(pos.x - (this_portal.size-1)/2), y=(pos.y+i), z=pos.z})
+				minetest.env:remove_node({x=(pos.x + (this_portal.size-1)/2), y=(pos.y+i), z=pos.z})
+				minetest.env:remove_node({x=(pos.x - (this_portal.size-1)/2+i), y=(pos.y+(this_portal.size-1)), z=(pos.z)})
+				minetest.env:remove_node({x=(pos.x - (this_portal.size-1)/2+i), y=(pos.y), z=pos.z})
+			end
+		else
+			for i=0,(this_portal.size-1) do
+				minetest.env:remove_node({x=pos.x, y=(pos.y+i), z=(pos.z - (this_portal.size-1)/2)})
+				minetest.env:remove_node({x=pos.x, y=(pos.y+i), z=(pos.z + (this_portal.size-1)/2)})
+				minetest.env:remove_node({x=pos.x, y=(pos.y+(this_portal.size-1)), z=(pos.z - (this_portal.size-1)/2+i)})
+				minetest.env:remove_node({x=pos.x, y=(pos.y), z=(pos.z - (this_portal.size-1)/2+i)})
+			end
+		end
+		if portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] ~= nil then
+			--portal_list[this_portal.exname].state = "deleted"
+			portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] = nil
+		else
+			halfportal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] = nil
+		end
+		entity.object:remove()
+		portals_changed = true
+	end
+	
+	function check_portal(pos,entity)
+		pos.x = math.floor(pos.x)
+		pos.y = math.floor(pos.y)
+		pos.z = math.floor(pos.z)
+		local this_portal = {}
+		if portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] ~= nil then
+			this_portal = portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+		else
+			this_portal = halfportal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+		end
+		if (this_portal.state == "built") then
+			if (this_portal.dir == 90)or(this_portal.dir == 270) then
+				minetest.env:add_node({x=pos.x, y=pos.y, z=pos.z}, {name="portal:rune_raido_g"})
+				minetest.env:add_node({x=pos.x, y=(pos.y+(this_portal.size-1)), z=pos.z}, {name="portal:rune_raido_g"})
+				minetest.env:add_node({x=(pos.x - (this_portal.size-1)/2), y=(pos.y + (this_portal.size-1)/2), z=pos.z}, {name="portal:rune_raido_g"})
+				minetest.env:add_node({x=(pos.x + (this_portal.size-1)/2), y=(pos.y + (this_portal.size-1)/2), z=pos.z}, {name="portal:rune_raido_g"})
+			else
+				minetest.env:add_node({x=pos.x, y=pos.y, z=pos.z}, {name="portal:rune_raido_g"})
+				minetest.env:add_node({x=pos.x, y=(pos.y+(this_portal.size-1)), z=pos.z}, {name="portal:rune_raido_g"})
+				minetest.env:add_node({x=pos.x, y=(pos.y + (this_portal.size-1)/2), z=(pos.z - (this_portal.size-1)/2)}, {name="portal:rune_raido_g"})
+				minetest.env:add_node({x=pos.x, y=(pos.y + (this_portal.size-1)/2), z=(pos.z + (this_portal.size-1)/2)}, {name="portal:rune_raido_g"})
+			end
+		end
+		local portal_broken = false
+		local dynpos = {}
+		local dynnodename = ""
+		local nnames = {}
+		if (this_portal.dir == 90)or(this_portal.dir == 270) then
+			for i=0,(this_portal.size-1) do
+				dynpos = {x=(pos.x - (this_portal.size-1)/2), y=(pos.y+i), z=pos.z}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+				dynpos = {x=(pos.x + (this_portal.size-1)/2), y=(pos.y+i), z=pos.z}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+				dynpos = {x=(pos.x - (this_portal.size-1)/2+i), y=(pos.y+(this_portal.size-1)), z=(pos.z)}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+				dynpos = {x=(pos.x - (this_portal.size-1)/2+i), y=(pos.y), z=pos.z}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+			end
+			for i,j in ipairs(nnames) do
+				if (j ~= rune_active) then
+					if (j ~= rune_otherstone) then
+						if (j ~= rune_unactive) then
+							portal_broken = true
+						end
+					end
+				end
+			end
+		else
+			for i=0,(this_portal.size-1) do
+				dynpos = {x=pos.x, y=(pos.y+i), z=(pos.z - (this_portal.size-1)/2)}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+				dynpos = {x=pos.x, y=(pos.y+i), z=(pos.z + (this_portal.size-1)/2)}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+				dynpos = {x=pos.x, y=(pos.y+(this_portal.size-1)), z=(pos.z - (this_portal.size-1)/2+i)}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+				dynpos = {x=pos.x, y=(pos.y), z=(pos.z - (this_portal.size-1)/2+i)}
+				table.insert(nnames,minetest.env:get_node(dynpos).name)
+			end
+			for i,j in ipairs(nnames) do
+				if (j ~= rune_active) then
+					if (j ~= rune_otherstone) then
+						if (j ~= rune_unactive) then
+							portal_broken = true
+						end
+					end
+				end
+			end
+		end
+		if portal_broken == true then
+			print("portal "..pos.x .."_" .. pos.y .. "_" .. pos.z.. " broken!")
+			if this_portal.state =="built" then
+				portal_list[this_portal.exname].state = "deleted"
+			end
+			
+			remove_portal(pos,entity)
+		end
+	end
+	
+	portal_control_ent = {
+		physical = false,
 		lastpos={},
-		visual = "cube",
-		initial_sprite_basepos = {x=0, y=0},
+		--initial_sprite_basepos = {x=0, y=0},
+		mydir = -1,
+		size = 0,
+		type ="",
+		owner = "",
+		exname = "no_exit!",
+		timer = 0,
+		state="",
 	}
 	
-	tp.on_punch = function(self, hitter)
-		hitter:setpos({x=115,y=3,z=-25})
+	portal_control_ent.on_activate = function(self, data)
+		local pos = self.object:getpos()
+			pos.x = math.floor(pos.x)
+			pos.y = math.floor(pos.y)
+			pos.z = math.floor(pos.z)
+		local this_portal = {}
+		
+		if portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] ~= nil then
+			this_portal = portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+		else
+			this_portal = halfportal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+		end
+		
+		if this_portal == nil then
+			self.object:remove()
+			print("portal ent selfremove!!")
+			return
+		end
+		self.dir = this_portal.dir
+		self.size = this_portal.size
+		self.type = this_portal.type
+		self.owner = this_portal.builder
+		self.exname = this_portal.exname
+		self.state = this_portal.state
+		check_portal(pos,self)
 	end
 	
-	tp.on_activate = function(self, data)
+	portal_control_ent.on_step = function(self, dtime)
+		self.timer=self.timer+dtime
+		local pos = self.object:getpos()
+			pos.x = math.floor(pos.x)
+			pos.y = math.floor(pos.y)
+			pos.z = math.floor(pos.z)
+			
+		if (self.timer>1)and(self.state ~= "deleted") then
+			self.timer = 0
+			local this_portal = {}
+			if portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z] ~= nil then
+				this_portal = portal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+			else
+				this_portal = halfportal_list[pos.x .."_" .. pos.y .. "_" .. pos.z]
+			end
+			self.dir = this_portal.dir
+			self.size = this_portal.size
+			self.type = this_portal.type
+			self.owner = this_portal.builder
+			self.exname = this_portal.exname
+			self.state = this_portal.state			
+			
+			local objs = minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y+(self.size-1)/2,z=pos.z}, self.size)
+			for k, obj in pairs(objs) do
+				if obj:get_player_name() ~= nil then
+					local player_pos = obj:getpos()
+					local player_tp = false
+					local dir_to_player = 0
+					local target_portal = portal_list[self.exname]
+					local new_player_pos = {}
+					local add_pos = 0
+					if (self.dir == 0)or(self.dir == 180) then
+						if 		((player_pos.x < (pos.x + (self.size-1)/2))and(player_pos.x > (pos.x - (self.size-1)/2))) and
+								((player_pos.y < (pos.y + (self.size)))and(player_pos.y > pos.y)) then
+							player_tp = true
+							dir_to_player = pos.x - player_pos.x
+							add_pos = pos.z - player_pos.z
+						end
+					end
+					if (self.dir == 90)or(self.dir == 270) then
+						if 		((player_pos.z < (pos.z + (self.size-1)/2))and(player_pos.z > (pos.z - (self.size-1)/2))) and
+								((player_pos.y < (pos.y + (self.size)))and(player_pos.y > pos.y)) then
+							player_tp = true
+							dir_to_player = pos.z - player_pos.z
+							add_pos = pos.x - player_pos.x
+						end
+					end
+					if (self.state == "built")and(player_tp==true) then
+						if (target_portal.dir == 90)or(target_portal.dir == 270) then
+							new_player_pos.z = target_portal.myz + 2*(add_pos / math.abs(add_pos))
+							new_player_pos.x = target_portal.myx + (dir_to_player)
+						else
+							new_player_pos.x = target_portal.myx + 2*(add_pos / math.abs(add_pos))
+							new_player_pos.z = target_portal.myz + (dir_to_player)
+						end
+						new_player_pos.y = (player_pos.y - pos.y) + target_portal.myy
+						print("player "..obj:get_player_name().." teleports from (" ..pos.x..","..pos.y..","..pos.z..") to ("..target_portal.myx..","..target_portal.myy..","..target_portal.myz..")")
+						obj:setpos(new_player_pos)
+					end
+				end
+			end
+			check_portal(pos,self)
+		end
+		if (self.state == "deleted") then
+			remove_portal(pos,self)
+		end
 	end
 	
-	tp.on_step = function(self, dtime)
-	end
-	
-	minetest.register_entity("t:tp", tp)
+	minetest.register_entity("portal:portal_control_ent", portal_control_ent)
 	
 	------------ Nodes ----------------
-	minetest.register_node("t:rune_raido_b", {
-	tile_images = {"default_cobble.png","default_cobble.png","default_cobble.png","raido_b.png","default_cobble.png","default_cobble.png"},
-	inventory_image = minetest.inventorycube("raido_b.png"),
-	is_ground_content = false,
-	material = minetest.digprop_dirtlike(1.0),
-	metadata_name = "generic",
-	dug_item = 'node "t:rune_raido_b" 1',
-})
+	minetest.register_node("portal:rune_raido_b", {
+		tile_images = {"default_cobble.png","default_cobble.png","raido_b.png","raido_b.png","raido_b.png","raido_b.png"},
+		inventory_image = minetest.inventorycube("raido_b.png"),
+		is_ground_content = false,
+		material = minetest.digprop_dirtlike(1.0),
+		dug_item = 'node "portal:rune_raido_b" 1',
+	})
+	minetest.register_node("portal:rune_raido_g", {
+		tile_images = {"default_cobble.png","default_cobble.png","raido_g.png","raido_g.png","raido_g.png","raido_g.png"},
+		inventory_image = minetest.inventorycube("raido_b.png"),
+		is_ground_content = false,
+		material = minetest.digprop_dirtlike(1.0),
+		dug_item = 'node "portal:rune_raido_b" 1',
+		light_source = 8,
+	})
 
 minetest.register_on_placenode(function(pos, newnode, placer)
-	if newnode.name == "t:rune_raido_b" then
-		local meta = minetest.env:get_meta(pos)
-		local n = meta:get_string("state1")
-		if n == nil then
-			meta:set_string("state1","o")
-			meta:set_infotext("o")
-		else 
-			meta:set_string("state1",n .. "o")
-			meta:set_infotext(n .. "o")
-		end
-		print("Raido!!")
+	if newnode.name == "portal:rune_raido_b" then
+		build_portal(pos,placer,5,rune_unactive,rune_otherstone)
 	end
 end)
+
+minetest.register_craft({
+	output = 'node "portal:rune_raido_b" 1',
+	recipe = {
+		{'node "default:sand"', 'node "default:wood"','node "default:cobble"'},
+		{'node "default:dirt"', 'node "default:mese" 5','node "default:jungletree"'},
+		{'node "default:cactus"','node "default:leaves"','node "default:tree"'}
+	}
+})
+
+print("[Portal " .. version .. "] Loaded!")
